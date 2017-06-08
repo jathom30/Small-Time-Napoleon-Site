fde3147e447aa2e9e6616721cd207bfe9f6a8c85
Jeff Thomas <jathom30@gmail.com>
2017-06-08 11:11:55 -0400

    updated file structure via brackets extension 'beautify'

--- js/mainPageScript.js
+++ js/mainPageScript.js
@@ -4,27 +4,25 @@ $('.section').children('div').hide();
 
 //hide name logo on scroll down, reveal name on scroll up
 var $nameLogo = $('.name-logo');
-
 $nameLogo.height()
-$(window).scroll(function() {
-    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    if (scrollTop > 200) {
-        $nameLogo.css('opacity',0);
-    }
-    else {
-        $nameLogo.css('opacity',1-scrollTop/200);
-    }
+$(window).scroll(function () {
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrollTop > 200) {
+    $nameLogo.css('opacity', 0);
+  } else {
+    $nameLogo.css('opacity', 1 - scrollTop / 200);
+  }
 }).scroll();
 
 //open selected div and close all others
 var sectionButton = document.querySelectorAll('.section h2');
-$(sectionButton).click(function() {
+$(sectionButton).click(function () {
 
   //get this clicked header and open its next sibling
   var $clicked = $(this).get(0);
   var opened = $clicked.nextSibling.nextSibling;
   $(opened).slideToggle();
-  
+
   //get parent of opened div and closes siblings children's divs
   var parent = opened.parentElement;
   var $others = $(parent).siblings();
@@ -43,27 +41,33 @@ $overlay.append($image);
 
 //add overlay
 $('body').append($overlay);
-  
+
 //a caption to overlay
 $overlay.append($caption);
 
 //capture click event on a link to an image
-$('#image-gallery a').click(function(event) {
+$('#image-gallery ul a').click(function (event) {
   event.preventDefault();
-  var imageLocation = $(this).attr('href');
-  //update overlay with the image linked in the link
-  $image.attr('src', imageLocation);
-  
-  //show the overlay
-  $overlay.show();
-  
+
   //add alt attribute as the caption
   var captionText = $(this).children('img').attr('alt');
   $caption.text(captionText);
+
+
+
+  var imageLocation = $(this).attr('href');
+  //update overlay with the image linked in the link
+  $image.attr('src', imageLocation);
+
+  //show the overlay
+  $overlay.show();
+
+
+
 });
-  
+
 //when overlay is clicked
-$overlay.click(function() {
+$overlay.click(function () {
   //hide overlay
   $overlay.hide();
 });
@@ -77,16 +81,14 @@ const $cancelPopUp = $('.cancel-pop-up');
 
 $popUp.hide();
 
-$popUp.ready(function() {
+$popUp.ready(function () {
   $popUp.fadeIn(1000);
 })
 
-$facebookLikeButton.click(function() {
+$facebookLikeButton.click(function () {
   window.location.href = 'http://facebook.com/smalltimenapoleonband';
 });
 
-$cancelPopUp.click(function() {
+$cancelPopUp.click(function () {
   $popUp.hide();
 });
-
-
